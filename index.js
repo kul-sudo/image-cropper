f929d6897bb17a0dd940d8867082712cc151f5e1
kul-sudo <ulyanov.kirill@protonmail.com>
2023-04-25 20:04:02 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// MAXIM EDITION
+
+import express from 'express'
+import sharp from 'sharp'
+import got from 'got'
+
+const PORT = 3000
+
+const app = express()
+
+app.get("/:width/:height/:url(*)", async (req, res) => {
+  try {
+    const { width, height, url } = req.params
+
+    const imageBuffer = await got(url).buffer()
+
+    const resizedImage = await sharp(imageBuffer)
+      .resize(Number(width), Number(height))
+      .toBuffer()
+
+    res.end(resizedImage)
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('An error occurred.');
+  }
+})
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
