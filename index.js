583c92e84384d9c9b21a97ad91fa438e8821a2d5
kul-sudo <ulyanov.kirill@protonmail.com>
2023-04-26 00:20:53 +0300

    d

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import sharp from 'sharp'
 import got from 'got'
-import { v4 } from 'uuid'
+import { join, resolve } from 'path'
 
 const PORT = 3000
+const dirname = resolve()
 
 const app = express()
 
@@ -11,16 +12,8 @@ app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`)
 })
 
-app.get('/api', (req, res) => {
-  const path = `/api/item/${v4()}`
-  res.setHeader('Content-Type', 'text/html')
-  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate')
-  res.end(`Hello! Go to item: <a href="${path}">${path}</a>`)
-})
-
-app.get('/api/item/:slug', (req, res) => {
-  const { slug } = req.params
-  res.end(`Item: ${slug}`)
+app.get('/', (req, res) => {
+  res.sendFile(resolve(dirname, 'static', 'index.html'))
 })
 
 app.get("/:width/:height/:url(*)", async (req, res) => {
